c865f8a54e2b381d299dbd11885198e4e0e771a3
Gianni Gambetti <gianni@gambetti.ca>
2022-01-31 19:25:33 -0500

    Add some EAN-13 support
    Also adds support to canonicalize variable weight codes.

--- upc.py
+++ upc.py
@@ -30,8 +30,12 @@ class UpcType(Enum):
     return UpcType.Normal
 
 
+# This handles EAN-13 codes...sort of.
+# Only because they mostly look like UPC-A.
 class Upc:
   BASE = 10 ** 6
+  MAX_UPC = 10 ** 12 - 1
+  MAX_EAN = 10 ** 13 - 1
 
   @staticmethod
   def decode(digits):
@@ -41,8 +45,36 @@ class Upc:
       l, r = divmod(digits, Upc.BASE)
       return Upc(l, r)
 
-  def encode(self):
-    return self.left * Upc.BASE + self.right
+  @staticmethod
+  def __checksum(d):
+      length = 12
+      if d > Upc.MAX_UPC:
+        # ie, it is EAN-13.
+        length = 13
+
+      digits = []
+      for i in range(length):
+        digits.append(d % 10)
+        d = d // 10
+      digits.reverse()
+
+      return (sum(digits[::2]) * 3 + sum(digits[1::2])) % 10
+
+  def encode(self, canonical=False):
+    # Return what was passed in by default.
+    if not canonical or self.type() != UpcType.VariableWeight:
+      return self.left * Upc.BASE + self.right
+
+    # Otherwise, only for VariableType, zero out the
+    # bottom bits & compute new valid checksum.
+    check = Upc.__checksum(self.left * Upc.BASE)
+
+    # Checksum digit changes weight (1 or 3) depending on the code length.
+    if self.left * Upc.BASE <= Upc.MAX_UPC:
+      return self.left * Upc.BASE + check
+
+    return self.left * Upc.BASE + ((check * 3) % 10)
+    
 
   def __init__(self, left, right):
     self.left = left
@@ -56,7 +88,11 @@ class Upc:
     return self.right % 10
 
   def _lead_digit(self):
-    return self.left // (Upc.BASE // 10)
+    # This is probably not correct for EAN-13...
+    firstish = self.left // (Upc.BASE // 10)
+    if firstish < 10:
+      return firstish
+    return firstish // 10
 
   def type(self):
     return UpcType.lookup(self._lead_digit())
@@ -64,16 +100,8 @@ class Upc:
   def check(self) -> bool:
     "Returns true if the UPC checksum is correct"
     if self._checksums is None:
-      digits = []
-      d = self.left * Upc.BASE + self.right
-      for i in range(12):
-        digits.append(d % 10)
-        d = d // 10
-      digits.reverse()
-
-      check = sum(digits[::2]) * 3 + sum(digits[1::2])
-
-      self._checksums = (check % 10) == 0
+      check = Upc.__checksum(self.left * Upc.BASE + self.right)
+      self._checksums = check == 0
 
     return self._checksums
 
