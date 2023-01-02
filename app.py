b1c4794d82bdcdfc88a4cf3caff69ad0b32033a8
Gianni Gambetti <gianni@gambetti.ca>
2023-01-02 15:26:07 -0500

    tasty-fare: refactor tracing setup
    don't enable tracing unless tracing settings exist in the config

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 # app.py updates
-    
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-# Initialize tracing and an exporter that can send data to Honeycomb
-provider = TracerProvider()
-processor = BatchSpanProcessor(OTLPSpanExporter())
-provider.add_span_processor(processor)
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
+def instrument_tracing(app, cfg):
+  from opentelemetry import trace
+  from opentelemetry.instrumentation.flask import FlaskInstrumentor
+  from opentelemetry.instrumentation.requests import RequestsInstrumentor
+  from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+  from opentelemetry.sdk.resources import Resource, SERVICE_NAME
+  from opentelemetry.sdk.trace import TracerProvider
+  from opentelemetry.sdk.trace.export import BatchSpanProcessor, SimpleSpanProcessor, ConsoleSpanExporter
+
+  r = Resource(attributes={
+    SERVICE_NAME: "tasty-fare",
+  })
+
+  # Initialize tracing and an exporter that can send data to Honeycomb
+  provider = TracerProvider(resource=r)
+  processor = BatchSpanProcessor(OTLPSpanExporter(
+      endpoint=cfg.get("endpoint", None),
+      headers=cfg.get("headers", None)
+  ))
+  #processor = SimpleSpanProcessor(ConsoleSpanExporter())
+
+  provider.add_span_processor(processor)
+  trace.set_tracer_provider(provider)
+  tracer = trace.get_tracer(__name__)
+
+  FlaskInstrumentor().instrument_app(app)
+  RequestsInstrumentor().instrument()
 
 # Initialize automatic instrumentation with Flask
 
@@ -21,5 +35,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
+tr = app.config.get("TRACING", None)
+if tr is not None:
+  instrument_tracing(app, tr)
